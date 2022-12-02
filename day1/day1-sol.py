dc82195bb8d5b093b8f5f68a6d5b1800a2c08df6
danieltruong <aduy1122@gmail.com>
2022-12-02 01:19:40 -0800

    Added file utils for parsing file input

--- day1/day1-sol.py
+++ day1/day1-sol.py
@@ -4,20 +4,19 @@ from typing import *
 script_dir = os.path.dirname(__file__)
 mymodule_dir = os.path.join(script_dir, '..')
 sys.path.append(mymodule_dir)
-from helpers import heapUtil
-
+from helpers import heapUtil, fileUtil
 
 def solution():
     res: List[int] = []
     currSum: int = 0
-    with open('data.txt', 'r') as f:
-        for lines in f:
-            converted_lines = str(lines).strip()
-            if converted_lines.strip():
-                currSum += int(converted_lines)
-            else:
-                res.append(currSum)
-                currSum = 0
+    data = fileUtil.textTo1DList('data.txt')
+    for lines in data:
+        converted_lines = str(lines).strip()
+        if converted_lines.strip():
+            currSum += int(converted_lines)
+        else:
+            res.append(currSum)
+            currSum = 0
     return heapUtil.nthLargestSum(3, res)
 
 print(solution())
